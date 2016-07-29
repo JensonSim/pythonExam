d9a14ad480cff51c7ffc6111268bed15840e58b4
JensonSim <bumkuns@gmail.com>
2016-07-30 00:25:59 +0900

    Update bShip.py
    잘돌아가지만 뭔가 좌표가 안맞음 ㅋㅋㅋㅋ 어쨋든 저장

--- bShip.py
+++ bShip.py
@@ -15,7 +15,7 @@ def random_row(board):
     return randint(0, len(board) - 1)
 
 def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    return randint(0, len(board) - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
@@ -26,17 +26,23 @@ guess_col = int(raw_input("Guess Col:"))
 
 if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sank my battleship!"
-       
-elif guess_row>5 or guess_row<0:
-    print "Outside the range"
-elif guess_col>5 or guess_col<0:
-    print "Outside the range"
+
+elif guess_row not in range(5) or guess_col not in range(5):
+    print "Oops,that's not even in the ocean."
 else:
-    print "You missed my battleship!"
-    board[guess_row][guess_col] = "X"
-    print ship_row
-    print ship_col
-print_board(board)
+    if guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col]== "X":
+        print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+        print ship_row
+        print ship_col
+    
+        print_board(board)
         
         
     
+
+# Write your code below!
