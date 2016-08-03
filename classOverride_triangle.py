6d18e104931f6e72e383afecb9168f660de5a1d9
JensonSim <bumkuns@gmail.com>
2016-08-03 16:13:26 +0900

    오류 : 새로만듬 삼각형 vs 정삼각형
    아빠클래스는 각도 참조변수를 세개받아야하는데
    
    자식클래스는 참조변수 필요없을때 
    
    오류발생중

---
+++ classOverride_triangle.py
@@ -0,0 +1,23 @@
+class Triangle(object):
+    number_of_sides = 3
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+        pass
+    
+    def check_angles(self):
+        if self.angle2 + self.angle2 +self.angle3 == 180:
+            return True
+        else:
+            return False
+            
+class Equilateral(Triangle):
+            	"""docstring for Equilateral"""
+            	angle = 60
+            	def __init__(self):
+            		super(Equilateral, self).__init__()
+            		self.angle1 = self.angle
+            		self.angle2 = self.angle
+            		self.angle3 = self.angle
+       
