e5b53b4766873b9f8b42dbc2b526b8d0bb1c9001
JensonSim <bumkuns@gmail.com>
2016-08-05 18:42:20 +0900

    해결했다,  return으로 인한 반복문이 작동을 안함

--- prime_mujui.py
+++ prime_mujui.py
@@ -12,17 +12,9 @@ def is_prime(x):
 				return a
 			else:
 				print x, "/", n, x, "is True"
-				return True
 
+	return True
 
-			
-
-print is_prime(3)
-print is_prime(4)
-
-print is_prime(7)
-
-print is_prime(9)
 
 
 
